c70713a9b4cdabeaccfeae5d667b8ecedefd5b6d
60manojkumar@gmail.com <Motivated@2019>
2023-01-20 19:48:24 +0530

    Initial commit

---
+++ src/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Demo {
+
+    public static void main(String args[]) {
+
+        System.out.println("This is my first java code");
+        System.out.println("Hello");
+
+
+    }
+}
