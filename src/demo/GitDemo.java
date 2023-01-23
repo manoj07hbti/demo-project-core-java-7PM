32bdeb83df377321b65e26a3e59b97e61589a205
60manojkumar@gmail.com <Motivated@2019>
2023-01-23 19:19:44 +0530

    This is demo for showing modified code

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("This is my GIT demo class");
+        System.out.println("This is my modification in same file ");
+
+        System.out.println("Adding new line to show green lines ");
     }
 }
