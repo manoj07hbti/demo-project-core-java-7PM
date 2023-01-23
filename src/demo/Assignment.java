45d6f7287437ceb2bdbd43dfe1ba28cdb42a46a4
60manojkumar@gmail.com <Motivated@2019>
2023-01-23 19:35:45 +0530

    Assignment 23 Jan

---
+++ src/demo/Assignment.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        String city="AGRA";
+
+        System.out.println("CIty name is : "+city);
+    }
+}
