7ab3db49dd2a0df63f85e75972b935a64453a551
60manojkumar@gmail.com <Motivated@2019>
2023-01-24 19:38:41 +0530

    Revision

---
+++ src/demo/DemoRevision.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class DemoRevision {
+
+    public static void main(String[] args) {
+
+       System.out.println("Demo revision");
+       System.out.println("This is my Java training ");
+
+    }
+}
---
+++ src/variable/VariableRevision.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class VariableRevision {
+
+    public static void main(String[] args) {
+
+        //SYNTAX : Datatype varName= value;
+
+        String cityName= "Agra";
+
+        int age=24;
+
+        double salary=34567.78;
+
+        System.out.println("City name : "+cityName);
+
+        System.out.println("Age : "+age);
+
+        System.out.println("Salary : "+salary);
+
+    }
+}
