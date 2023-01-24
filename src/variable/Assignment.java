9e163420e42ba07ec6326ea1ab128753b19615ac
60manojkumar@gmail.com <Motivated@2019>
2023-01-24 20:01:24 +0530

    Assignment 24 Jan

---
+++ src/variable/Assignment.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+
+
+        String course="Java Microservice Developer";
+
+        int number=3;
+
+        int square= number*number;
+
+        System.out.println("Square of 3 is "+square);
+
+        int number_1=120;
+        int percentage=11;
+
+        double result= (double) (number_1*percentage)/100;
+
+        System.out.println("result is "+result);
+
+
+    }
+}
