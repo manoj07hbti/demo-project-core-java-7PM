1b29a9d0b1477035a61f8ae30ec333f1471f9c4d
AjaySaini1999 <arayansaini1999@gmail.com>
2023-01-25 01:44:34 +0530

    First_Commit

---
+++ src/variable/VariableClass.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class VariableClass {
+    public static void main(String[] args){
+        int age=24;
+        String name="Praveen Kumar";
+        int hight=172;
+        String mob="8923286033";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Hight:"+hight);
+        System.out.println("Mobile Number:"+mob);
+    }
+}
