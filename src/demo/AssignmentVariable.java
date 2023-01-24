76c8c87fdcc161420598617225e44f493a9afe58
60manojkumar@gmail.com <Motivated@2019>
2023-01-24 19:32:48 +0530

    Assignment 24 Jan

---
+++ src/demo/AssignmentVariable.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class AssignmentVariable {
+
+    public static void main(String[] args) {
+
+
+        String name="Java";
+        System.out.println("name is : "+name);
+    }
+}
