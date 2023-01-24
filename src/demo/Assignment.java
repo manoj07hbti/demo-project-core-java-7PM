969a12a01e1509cab93001b1201e17e66091ff6a
Vishal9528 <vishal8954198936@gmail.com>
2023-01-24 18:56:31 +0530

    Assignment 24 Jan.

---
+++ src/demo/Assignment.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Assignment {
+    public static void main(String[] args) {
+
+        int number=1584;
+        int Totel=2400;
+        int result=(number*100/Totel);
+
+        System.out.println("1584 is what % of 2400=" +result);
+
+
+    }
+}
