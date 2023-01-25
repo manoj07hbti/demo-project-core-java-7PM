c88736ffb9b86a8e2690a71f1a7c87fab3343d45
Vishal9528 <vishal8954198936@gmail.com>
2023-01-25 18:57:24 +0530

    Assignment 25 Jan.

---
+++ src/assignment/Assignment1st.java
@@ -0,0 +1,25 @@
+package assignment;
+
+public class Assignment1st {
+    public static void main(String[] args) {
+
+        String Name = ("Atul");
+        String DOB = ("20/02/1997");
+        String Qualification = ("Engineering");
+
+        System.out.println("Name= " +Name);
+        System.out.println("D.O.B= " +DOB);
+        System.out.println("Qualification= " +Qualification);
+
+        String Bank_Name = ("SBI");
+        String Branch = ("Agra");
+        String Customer_Name = ("Atul");
+        long Account_No = 10000352678907l;
+
+        System.out.println("Bank Name= " +Bank_Name );
+        System.out.println("Branch= " +Branch);
+        System.out.println("Customer Name= " +Customer_Name);
+        System.out.println("Account No.= " +Account_No);
+
+    }
+}
