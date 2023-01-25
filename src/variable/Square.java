fc663f485eca71fe0d7c6ca27d321644ae338d46
60manojkumar@gmail.com <Motivated@2019>
2023-01-25 19:19:06 +0530

    Square of number

---
+++ src/variable/Square.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Square {
+
+    public static void main(String[] args) {
+
+       int number=3;
+
+       int square= number*number;
+
+       System.out.println("Square of number is : "+square);
+
+    }
+}
