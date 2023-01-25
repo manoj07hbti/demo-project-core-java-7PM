9cd92c8310396360cb8a64e608f4abc4c7defff6
amandev2023 <devboyzzz@gmailcom>
2023-01-25 20:07:57 +0530

    Assignment of 24-jan

---
+++ src/variable/AssignmentJan24.java
@@ -0,0 +1,31 @@
+package variable;
+
+public class AssignmentJan24 {
+    public static void main(String args[]) {
+
+        int a = 3;
+        int b = 5;
+        int c = 100;
+        int d = 120;
+        int e = 10;
+
+        // Find the square of 3 //
+
+        int multi = a * a;
+        System.out.println("Square of 3 is : " + multi);
+
+        //Find the cube of 5//
+
+        int multiply = b * b * b;
+        System.out.println("Cube of 5 is : " + multiply);
+
+        //Find the percent of given number//
+        //Find 10% of 120??//
+        int number = 120;
+        int percent = 10;
+        int result = (number * percent) / 100;
+        System.out.println("10 percent of 120 is : " + result);
+
+
+    }
+}
