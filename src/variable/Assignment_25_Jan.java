da4b17b9829ae4918f12119e38d3303d9439bc1f
Ashish <vermaashish732@gmail.com>
2023-01-25 20:13:24 -0800

    first assignment

---
+++ src/variable/Assignment_25_Jan.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Assignment_25_Jan {
+    public static void main(String[] args) {
+        System.out.println("WAP to print country, population, number of states");
+        String country;
+        country= "india";
+        double pop= 1250000000;
+        int states= 28;
+        System.out.println("country = " +country);
+        System.out.println("population = " +pop);
+        System.out.println("number of states = " +states);
+
+    }
+}
