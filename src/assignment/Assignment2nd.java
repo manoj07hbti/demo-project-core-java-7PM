263ecefa1f2c4c408a75e12a240631fef666288b
Vishal9528 <vishal8954198936@gmail.com>
2023-01-26 18:26:50 +0530

    Assignment 26 Jan.

---
+++ src/assignment/Assignment2nd.java
@@ -0,0 +1,31 @@
+package assignment;
+
+public class Assignment2nd {
+    public static void main(String[] args) {
+
+        String Country=("INDIA");
+        String Population=("1.39 Billion");
+        int Number_of_States=28;
+
+        System.out.println("Country:- " +Country);
+        System.out.println("Population:- " +Population);
+        System.out.println("Number of States:- " +Number_of_States);
+
+        int a=5;
+        int Multiply=a*a*a;
+
+        System.out.println("Cube of 5:- " +Multiply);
+
+        double Number=34550;
+        int Percent=5;
+        double Ans= (double)(Number*Percent/100);
+
+        System.out.println("5% of 34550:- " +Ans);
+
+        int b=5;
+        int c=2;
+        double Result=(double) b/c;
+
+        System.out.println("Divid:- " +Result);
+    }
+}
