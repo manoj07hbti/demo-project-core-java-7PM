4d03a8f8f1f45856dbc0109b6806b93292011121
corejava <imranaoa@gmail.com>
2023-01-26 03:01:48 -0800

    square root

--- src/izyan/hpi3.java
+++ src/izyan/hpi3.java
@@ -4,40 +4,43 @@ public class hpi3 {
     public static void main(String[] args) {
         String cityName;
 
-        cityName="Firozabad";
+        cityName = "Firozabad";
 
         System.out.print(cityName);
-        System.out.println("city"+cityName);
+        System.out.println("city" + cityName);
 
         int age;
-        age=40;
+        age = 40;
         System.out.println(age);
-        System.out.println("Age:"+age);
+        System.out.println("Age:" + age);
 
         double marks;
-        marks=90.5;
+        marks = 90.5;
 
         System.out.println("marks");
-        System.out.println("marks" +marks);
+        System.out.println("marks" + marks);
 
         String length;
-        length="7.7 inches";
+        length = "7.7 inches";
 
-        System.out.println("Length is: "+length);
+        System.out.println("Length is: " + length);
 
-        String company ="hp";
+        String company = "hp";
 
-        System.out.println("Industry"  +company);
+        System.out.println("Industry" + company);
 
         String brand;
 
-        brand="vivo";
-        System.out.println("i am using this brand mobile:"+brand);
+        brand = "vivo";
+        System.out.println("i am using this brand mobile:" + brand);
 
+        int five=5;
+        int result=five*five;
+        System.out.println(result);
 
-
-
-
+        int three=3;
+        int output=three*three;
+        System.out.println(output);
 
     }
 }
