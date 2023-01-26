0e149da267ee303f61c8e3d3858c9163f8116870
Ashish <vermaashish732@gmail.com>
2023-01-25 19:32:50 -0800

    first assignment

---
+++ src/variable/Ashish_Assignment.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Ashish_Assignment {
+    public static void main(String args[]){
+        System.out.println("Hello");
+        System.out.println("WAP to print cube of a number");
+        int a=5;
+        int b= a*a*a;
+        System.out.println("cube of the number is: " +b);
+    }
+
+}
