168ef20168dce1df63e4ec9934dd582eaf564b0e
Syed <syed5b4@gmail.com>
2023-01-26 10:28:24 +0530

    Assignmet 25 jan

---
+++ src/variable/Assignment_25_Jan.java
@@ -0,0 +1,29 @@
+package variable;
+
+public class Assignment_25_Jan {
+
+    public static void main (String arg[]){
+
+        //syntax: print country population and states
+
+        String CountryName="India";
+        int states=29;
+        Double population =1250000.00;
+        System.out.println("COUNTARY NAME: "+  CountryName  + "  POPULATION : " +  population  +  "  STATES : "  +  states);
+
+
+        //syentax : cube of 5
+
+        int Number=5;
+        int Cube = Number*Number*Number;
+        System.out.println(" CUBE OF 5 IS : "  +Cube);
+
+
+        //5 Percentace of  given  34550
+
+        double Num= 34450, percentage = 5, result;
+        result=Num*percentage/100;
+        System.out.println(" RESULT " +result);
+
+    }
+}
