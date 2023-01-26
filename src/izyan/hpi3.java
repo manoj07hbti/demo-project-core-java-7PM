a5e40dff73b1c7d90d29bd4618068e0a5f807683
corejava <imranaoa@gmail.com>
2023-01-26 03:38:07 -0800

    new change

---
+++ src/izyan/hpi3.java
@@ -0,0 +1,43 @@
+package izyan;
+
+public class hpi3 {
+    public static void main(String[] args) {
+        String cityName;
+
+        cityName="Firozabad";
+
+        System.out.print(cityName);
+        System.out.println("city"+cityName);
+
+        int age;
+        age=40;
+        System.out.println(age);
+        System.out.println("Age:"+age);
+
+        double marks;
+        marks=90.5;
+
+        System.out.println("marks");
+        System.out.println("marks" +marks);
+
+        String length;
+        length="7.7 inches";
+
+        System.out.println("Length is: "+length);
+
+        String company ="hp";
+
+        System.out.println("Industry"  +company);
+
+        String brand;
+
+        brand="vivo";
+        System.out.println("i am using this brand mobile:"+brand);
+
+
+
+
+
+
+    }
+}
