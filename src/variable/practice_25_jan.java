e0aa737d1acd66fa3106aeae7545c14aa25d0429
Ashish <vermaashish732@gmail.com>
2023-01-25 21:22:15 -0800

    first assignment

--- src/variable/practice_25_jan.java
+++ src/variable/practice_25_jan.java
@@ -5,19 +5,26 @@ public class practice_25_jan {
     public static void main(String[] args) {
         System.out.println("practice 25 Jan");
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter marks of subject 1 = ");
-        int a= sc.nextInt();
-        System.out.println("Enter marks of subject 2 = ");
-        int b= sc.nextInt();
-        System.out.println("Enter marks of subject 3 = ");
-        int c= sc.nextInt();
-        System.out.println("Enter marks of subject 4 = ");
-        int d= sc.nextInt();
-        System.out.println("Enter marks of subject 5 = ");
-        int e= sc.nextInt();
-        int sum= a+b+c+d+e;
-        int per = sum/5;
-        System.out.println("percentage of the numbers = " +per);
+        //  System.out.println("Enter marks of subject 1 = ");
+        //  int a= sc.nextInt();
+        //  System.out.println("Enter marks of subject 2 = ");
+        //   int b= sc.nextInt();
+        //  System.out.println("Enter marks of subject 3 = ");
+        //   int c= sc.nextInt();
+        //  System.out.println("Enter marks of subject 4 = ");
+        //   int d= sc.nextInt();
+        //  System.out.println("Enter marks of subject 5 = ");
+        //  int e= sc.nextInt();
+        //  int sum= a+b+c+d+e;
+        //  int per = sum/5;
+        //  System.out.println("percentage of the numbers = " +per);
+        // String name;
+        // name = "Ashish";
+        // System.out.print("Hello " +name +" Have a good day!");
+        System.out.println("WAP to convert kilometers into miles");
+        double km= 450;
+        double miles= km*0.621371;
+        System.out.println("distance in miles = " +miles);
 
     }
 }
