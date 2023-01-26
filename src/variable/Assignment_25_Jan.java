d6d1a360accd0f8656c6fe26d288c1a327928c8f
Ashish <vermaashish732@gmail.com>
2023-01-25 20:24:48 -0800

    first assignment

--- src/variable/Assignment_25_Jan.java
+++ src/variable/Assignment_25_Jan.java
@@ -4,12 +4,24 @@ public class Assignment_25_Jan {
     public static void main(String[] args) {
         System.out.println("WAP to print country, population, number of states");
         String country;
-        country= "india";
+        country= "India";
         double pop= 1250000000;
         int states= 28;
         System.out.println("country = " +country);
         System.out.println("population = " +pop);
         System.out.println("number of states = " +states);
+        System.out.println("=========================");
+        System.out.println("WAP to print cube of 5");
+        int a=5;
+        int b;
+        b = a*a*a;
+        System.out.println("cube of a number =" +b);
+        System.out.println("=========================");
+        System.out.println("WAP to print percentage of a number = ");
+        int number = 34550;
+        int per= number*5/100;
+        System.out.println("percentage of a number is = " +per);
+
 
     }
 }
