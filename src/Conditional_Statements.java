d22d980146c00346d43ae9d0f5ae927f1b6384e9
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-01-27 19:41:19 +0530

    Assignment of 27 January

---
+++ src/Conditional_Statements.java
@@ -0,0 +1,60 @@
+public class Conditional_Statements {
+    public static void main(String[] args) {
+
+        int Mark=33;
+        if (Mark<34){
+
+            System.out.println("Student Pass the Exam");
+
+            int marks=55;
+            if (marks>54){
+
+                System.out.println("Student passed the exam with second devision");
+
+                int Marks=60;
+                if (Marks>59){
+
+                    System.out.println("Student cleared exam with first devision");
+
+
+                    int Age=17;
+                    if (Age<18){
+
+                        System.out.println("Person is child");
+
+                        int age=44;
+                        if (age<45){
+
+                            System.out.println("Person is young");
+
+                            int Age1=61;
+                            if (Age1>60){
+
+                                System.out.println("Person Is Old");
+                            }
+
+
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
