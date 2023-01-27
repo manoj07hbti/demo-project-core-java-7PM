96a28ef4dc8b102e4d1e302e5fabbd89d7228cde
Ashish <vermaashish732@gmail.com>
2023-01-27 17:57:29 +0530

    first assignment

--- src/variable/practive_27_jan.java
+++ src/variable/practive_27_jan.java
@@ -3,11 +3,50 @@ import java.util.Scanner;
 
 public class practive_27_jan {
     public static void main(String[] args) {
-        System.out.println("enter number");
+//        System.out.println("enter number");
+//        Scanner sc= new Scanner(System.in);
+//        boolean b1= sc.hasNextInt();
+//        System.out.println("entered number is integer or not: ");
+//        System.out.println("for integer: True    For not integer: false ");
+//        System.out.println(b1);
+
+//        System.out.print("enter distance in kilometers: ");
+//        Scanner sc= new Scanner(System.in);
+//        float km= sc.nextFloat();
+//        float miles = km*0.6222f;
+//        System.out.print("distance in miles: " +miles);
         Scanner sc= new Scanner(System.in);
-        boolean b1= sc.hasNextInt();
-        System.out.println("entered number is integer or not: ");
-        System.out.println("for integer: True    For not integer: false ");
-        System.out.println(b1);
+
+//        System.out.print("Enter the value of x : ");
+//        int x = sc.nextInt();
+//        System.out.print("Enter the value of y : ");
+//        int y = sc.nextInt();
+//        float result= (x-y)/2f;
+//        System.out.println("result= (x-y)/2= " +result);
+
+//        System.out.print("Enter the value of a : ");
+//        float a= sc.nextFloat();
+//        System.out.print("Enter the value of b : ");
+//        float b= sc.nextFloat();
+//        System.out.print("Enter the value of c : ");
+//        float c= sc.nextFloat();
+//        float result= ((b*b)-4*a*c)/(2*a);
+//        System.out.println("result = (b*b-4ac)/2a : " +result);
+//        System.out.print("enter first alphabet : ");
+//        char a = sc.next().charAt(0);
+//        a++;
+//        System.out.println("Next aphabet : " +a);
+//        System.out.println("a= 7/4*9/2");
+//        float a = (7/4f*9/2f);
+//        System.out.println(a);
+//        System.out.println("Enter your grade: ");
+//        float grade= sc.nextFloat();
+//        float egrade= grade + 8;
+//        System.out.println("your encrypted grade : " + egrade);
+//        egrade =egrade -8;
+//        System.out.println("your decrypted grade : " + egrade);
+        int a = 7*49/7+35/7;
+        System.out.println(a);
+
     }
 }
