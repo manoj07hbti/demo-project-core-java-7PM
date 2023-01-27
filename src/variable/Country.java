1c10da5484048a536c6aa02cf6da84d32968f49c
AjaySaini1999 <arayansaini1999@gmail.com>
2023-01-27 19:46:53 +0530

    First_Commit

---
+++ src/variable/Country.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Country {
+    public static void main(String[] args){
+        String country = "India",population ="1.43 B", Num="36";
+        System.out.println("Country: "+country+'\n'+"Population: "+population+'\n'+"Number os States: "+Num);
+    }
+}
---
+++ src/variable/Percentage.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Percentage {
+    public static void main(String[] args){
+        double Number= 34550,percent=5,result;
+        result=Number*percent/100;
+        System.out.println("5% of 34550 is "+result);
+    }
+}
