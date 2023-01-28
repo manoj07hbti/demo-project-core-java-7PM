265d8d6bc6c56f1fecb3190d4d86033f8ccfe21a
Krishna Varshney <yadavseeta168@gmail.com>
2023-01-29 00:44:30 +0530

    demo java 2

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -1,6 +1,35 @@
 package demo;
 
 public class Demo2 {
+    public static void main(String args[]){
+        // find the squre of 4//
+        int s=4;
+        int t=6;
+        int u=50;
+        int v=100;
+        int w=20;
+
+        int multi=s*s;
+
+        System.out.println("squre of 4"+multi);
+
+       double number=120,  parcent=20;
+
+       int precent=v/w;
+       System.out.println("20/100"+precent);
+
+       //find the cube of 6//
+          int multiply=t*t*t;
+         System.out.println("cube of 6"+multiply);
+
+
+
+
+
+
+
+
+    }
 
 
 }
