706896c4db562b9a1dbff791e3ff42b517eda18c
Krishna Varshney <yadavseeta168@gmail.com>
2023-01-29 00:20:36 +0530

    Assigment Varible 1

---
+++ src/AssigmentVarible/Assigment1.java
@@ -0,0 +1,68 @@
+package AssigmentVarible;
+
+public class Assigment1 {
+    public static void main(String args []) {
+        String Country = ("India");
+        System.out.println("Country name is"+Country);
+
+        String population=("2.32");
+        System.out.println("population"+population);
+
+        int number_of_state=(28);
+        System.out.println("state_of_number"+number_of_state);
+
+         int d=6;
+         int multiply=d*d*d;
+         System.out.println("Cube of squre"+multiply);
+
+         double number=33456;
+         int parcent=6;
+         double Ans=(double)(number*parcent/100);
+         System.out.println("5%33456"+Ans);
+
+
+
+
+
+
+;
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/ConditionStatement/conditionIfelse.java
@@ -0,0 +1,4 @@
+package ConditionStatement;
+
+public class conditionIfelse {
+}
