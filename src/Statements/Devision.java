1a8fe6d9d759837f868b3a17b928c5f7b962d8a0
Praveen Kumar <66659885+PraveenRaj00@users.noreply.github.com>
2023-01-28 19:36:17 +0530

    Division Statement

---
+++ src/Statements/Devision.java
@@ -0,0 +1,14 @@
+package Statements;
+
+public class Devision {
+    public static void main(String[] args){
+        int num=21;
+
+        if(num%5==0){
+            System.out.println("Number is Devisival by 5");
+        }
+        else{
+            System.out.println("Number is not Devisival by 5");
+        }
+    }
+}
