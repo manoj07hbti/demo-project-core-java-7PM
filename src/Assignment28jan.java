65d38fe0e16a569eabe77283bc9ae8e766c4ba6c
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-01-30 01:51:07 +0530

    Assignment of 25 January

---
+++ src/Assignment28jan.java
@@ -0,0 +1,43 @@
+public class Assignment28jan {
+    public static void main(String[] args) {
+
+        //Assignment No.1...//
+
+        int number=20;
+        if (number>5){
+            if (number %5 ==0){
+                System.out.println("number is devesible by 5");
+            }
+        }
+        else {
+            System.out.println("number is not devesible by 5");
+        }
+
+        // Assignment No.2...//
+
+        String country="India";
+        if (country == "India"){
+            System.out.println("my country is India");
+
+        }
+
+        else {
+            System.out.println("I am Foreighner");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
