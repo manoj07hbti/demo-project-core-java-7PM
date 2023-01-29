df83f9b7502f265907667befbf2594b1d0bf4c50
Krishna Varshney <yadavseeta168@gmail.com>
2023-01-30 00:41:27 +0530

    demo project if else

--- src/ConditionStatement/conditionIfelse.java
+++ src/ConditionStatement/conditionIfelse.java
@@ -1,4 +1,26 @@
 package ConditionStatement;
 
 public class conditionIfelse {
+    public static void main(String args[]) {
+        String Country = "India"; // My Country is India//
+        if (Country == "India") {
+            System.out.println("My Country is India");
+
+        } else {
+            System.out.println("my Country  is Foriegner");
+
+        }
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
