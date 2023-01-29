e2df826fa9dbcb58bf696f082e92fad6b74b7678
aanchalatre2014 <aanchalatre2014@gmail.com>
2023-01-29 21:43:23 +0530

    conditional statements

---
+++ src/variable/Conditionalstatements.java
@@ -0,0 +1,33 @@
+package variable;
+
+public class Conditionalstatements {
+    public static void main(String[] args) {
+
+                int marks=40;
+                if(marks>34){
+                    System.out.println("Student cleared exam with passing marks");
+
+                }
+                marks=60;
+                if(marks>55){
+                    System.out.println("Student cleared exam with second class");
+                }
+                marks=70;
+                if (marks>60){
+                    System.out.println("student cleared exam with first class");
+                }
+                int age= 15;
+                if(age<18){
+                    System.out.println("person is a child");
+                }
+                age=45;
+                if(age<47){
+                    System.out.println("Person is young");
+                }
+                 age=60;
+                if(age<63){
+                    System.out.println("Person is old");
+                }
+    }
+}
+
