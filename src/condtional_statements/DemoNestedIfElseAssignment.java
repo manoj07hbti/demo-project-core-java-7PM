faf7b346dd5d49738341dadd1ab4e09f6532bc65
Vishal9528 <vishal8954198936@gmail.com>
2023-01-30 19:18:20 +0530

    Assignment 30 Jan.

--- src/condtional_statements/DemoNestedIfElseAssignment.java
+++ src/condtional_statements/DemoNestedIfElseAssignment.java
@@ -5,9 +5,7 @@ public class DemoNestedIfElseAssignment {
 
         int number = 50;
 
-        if (number != 0) {
-
-            if (number / 5 == 0) {
+        if (number % 5 == 0) {
 
                 System.out.println("Number is Divisible by 5");
             }
@@ -16,10 +14,8 @@ public class DemoNestedIfElseAssignment {
                 System.out.println("Number is not divisible by 5");
             }
 
-        }
 
-
-           String country = "PAKISTAN";
+            String country = "PAKISTAN";
 
             if (country == "INDIA") {
 
