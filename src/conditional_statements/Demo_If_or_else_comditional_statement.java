ba01607d1cc548b42ee74b7b8d980c808ba25d15
amandev2023 <devboyzzz@gmailcom>
2023-01-30 18:49:36 +0530

    Assignment 28Jan(Conditional_Statement_If or Else_)

---
+++ src/conditional_statements/Demo_If_or_else_comditional_statement.java
@@ -0,0 +1,27 @@
+package conditional_statements;
+
+public class Demo_If_or_else_comditional_statement {
+    public static void main(String[] args){
+
+        // 1.Write a code to check any number is divisible by 5//
+
+        int number;
+        number = 34;
+        if (number % 5 == 0) {
+            System.out.println("number is divisible by 5");
+        } else {
+            System.out.println("Number is not divisible by 5");
+        }
+
+        // 2.Write a code if country is India print INDIAN else FOREIGNER//
+
+        String country;
+        country="India";
+        if(country=="India"){
+            System.out.println("Indian");
+        }
+        else{
+            System.out.println("FOREIGNER");
+        }
+    }
+}
