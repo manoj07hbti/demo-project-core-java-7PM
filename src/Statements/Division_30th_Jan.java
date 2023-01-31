8b3291944d34cc63cff02f3c85949efb3dc30883
Praveen Kumar <66659885+PraveenRaj00@users.noreply.github.com>
2023-02-01 00:09:42 +0530

    Assignment 30th & 31st Jan

---
+++ src/Statements/Division_30th_Jan.java
@@ -0,0 +1,24 @@
+package Statements;
+
+public class Division_30th_Jan {
+    public static void main(String[] args){
+        //Calculating Division
+
+        int marks=33;
+
+        if(marks>=33 & marks<44){
+            System.out.println("Pass with Third Division");
+        }
+        if(marks>44 & marks<60){
+            System.out.println("Pass with Second Division");
+        }
+        if(marks>=60) {
+            System.out.println("Pass with First Division");
+        }
+        if(marks<33) {
+            System.out.println("Fail");
+        }
+
+
+    }
+}
