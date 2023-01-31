88c99fef6a6113dd4310d15282230b44b149b3b6
ravind0303 <ravind.pcf@gamil.com>
2023-01-31 20:28:02 +0530

    Conditional Statement

---
+++ src/conditional_Statement/If_Conditional_Statement.java
@@ -0,0 +1,23 @@
+package conditional_Statement;
+
+public class If_Conditional_Statement {
+
+    public static void main(String[] args) {
+
+        int age;
+         age=21;
+
+         if (age>18){
+
+             System.out.println("Person is eligible for voting "+age);
+         }
+
+         if (age<18) {
+
+
+             System.out.println("Person is not eligible for voting " + age);
+         }
+    }
+
+
+}
