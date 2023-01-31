b563fd8dca6915c3c833583282a4ddeeeda5b531
Vishal9528 <vishal8954198936@gmail.com>
2023-01-31 05:51:28 +0530

    IF,ELSE Assignment

---
+++ src/condtional_statements/DemoIfElseAssignment.java
@@ -0,0 +1,33 @@
+package condtional_statements;
+
+public class DemoIfElseAssignment {
+    public static void main(String[] args) {
+
+
+        int number = 44;
+
+        if (number % 5 == 0) {
+
+            System.out.println("Number is divisible by 5");
+        }
+
+        else {
+
+            System.out.println("Number is not divisible by 5");
+        }
+
+        String country = "AMERICA";
+
+        if (country == "INDIA") {
+
+            System.out.println("Person belongs to INDIA");
+        }
+
+        else {
+
+            System.out.println("Person belongs to FOREIGNER");
+        }
+
+    }
+}
+
