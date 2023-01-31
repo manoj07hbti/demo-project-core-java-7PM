7ea973703dca6cfeb670a90c270866e529f09c71
DELL <kumargaurav23097@gmail.com>
2023-01-31 20:11:26 +0530

    Use IF Block

---
+++ src/conditional_statement/DemoIf.java
@@ -0,0 +1,37 @@
+package conditional_statement;
+
+public class DemoIf {
+
+    public static void main (String []args){
+
+        int age;
+
+
+        age=19;
+
+        if(age>18){
+
+            System.out.println("Person IS Eligible For Voting");
+
+        }
+        if(age<18){
+
+            System.out.println("Person Is NOt Eligible For Voting ");
+        }
+        String city="Agra";
+
+        if(city==" Agra"){
+            System.out.println("Person Is BelongTo Agra");
+
+        }
+        if(city=="Delhi"){
+            System.out.println("Person Belong TO Some Other City");
+        }
+
+
+    }
+
+
+
+
+}
