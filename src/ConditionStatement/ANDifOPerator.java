3a7a0174463de47607836f6ef01fd4b1e262e92f
Krishna Varshney <yadavseeta168@gmail.com>
2023-01-31 23:46:37 +0530

    And if operetor condition

---
+++ src/ConditionStatement/ANDifOPerator.java
@@ -0,0 +1,57 @@
+package ConditionStatement;
+
+public class ANDifOPerator {
+    public static void main(String args[]){
+        int age;
+        String city;
+        age=19; city="PUNE";
+
+        if ( age>18 & city=="PUNE") {
+
+            //TRUE & TRUE= TRUE//
+
+            System.out.println("Persan is eligible for votingin  in Metro city");
+        }else{
+            System.out.println("Person is not eligible for voting in Metro city");
+        }
+        //TRUE &  FALSE= FALSE//
+        age=19;
+        city="DELHI";
+
+        if(age>18 & city=="Chennai") {
+
+            System.out.println("Persan is eligible for voting in Merto city");
+        }else{
+            System.out.println("Persan is  not eligible for voting in Metro city ");
+        }
+        //FALSE & TRUE=FALSE//
+
+         age=16;
+        city="Raipur";
+        if(age>18  & city=="Raipur"){
+
+            System.out.println("Persan is eligible for voting in Metro city");
+        }else{
+
+            System.out.println("persan is eligible for voting in Metro city");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
