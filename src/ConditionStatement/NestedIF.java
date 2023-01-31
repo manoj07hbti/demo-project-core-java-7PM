ae60c0057a7bef2c8d84788063a298f0e0d85001
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-01 00:04:23 +0530

    Condition NESTED if java

--- src/ConditionStatement/NestedIF.java
+++ src/ConditionStatement/NestedIF.java
@@ -1,4 +1,31 @@
 package ConditionStatement;
 
 public class NestedIF {
-}
+    public static void main(String args []){
+
+        int number=15;
+        if( number>0){
+
+             if (number % 5== 0){
+
+               System.out.println("Number is Even");
+
+             }
+
+
+        } else{
+            System.out.println("Number is odd");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
