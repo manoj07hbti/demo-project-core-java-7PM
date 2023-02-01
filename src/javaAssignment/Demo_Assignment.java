48fd6af1e6e1aab95a88928493df50a1877b3147
pc user <paramvirsingh127@gmail.com>
2023-02-01 23:42:03 +0530

    Assignment of 25 january 2023

---
+++ src/javaAssignment/Demo_Assignment.java
@@ -0,0 +1,35 @@
+package javaAssignment;
+
+public class Demo_Assignment {
+    public static void main(String[] args) {
+        //SYNTEX : varname
+        // variable declaration
+        String cityname; // we have declare a strong variable with cityname
+        cityname ="Agra"; // this assignment of tuesday 24 jan
+
+        System.out.println("city name is"+cityname);
+
+        int age;
+        age=35; // we have assigned 35 value of age variable
+
+        System.out.println("age of"+age);
+
+// this assignment of 25 jan
+        int number_2 =5;
+        int square= number_2*number_2;
+        System.out.println("Square of 5 is="+square);
+
+        int number=6;
+        int cube = number*number*number;
+        System.out.println("Cube of 6 is"+cube);
+
+
+        int number_1 = 34550;
+        int percentage = 5;
+
+        double result = (double)(number_1 * percentage) / 100;
+
+        System.out.println("result of percentage is" + result);
+
+    }
+}
