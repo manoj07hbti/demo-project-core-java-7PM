f4ca8767537699e41ca318019dade3e7b3901885
DELL <kumargaurav23097@gmail.com>
2023-02-01 13:38:16 +0530

    Variable

---
+++ src/variable/Demo_Variable.java
@@ -0,0 +1,26 @@
+package variable;
+
+public class Demo_Variable {
+
+    public static void main(String[]args){
+
+        // Syntex :  Datatype Variable //
+
+        String name;   //   varName : Value //
+
+        name="Shiva";
+        System.out.println("Person Name Is : "+name);
+
+        int age;
+        age=21;
+        System.out.println("Person Age Is : "+age);
+
+        double height;
+        height=1.74;
+        System.out.println("Person Height Is : "+height);
+
+    }
+
+
+
+}
