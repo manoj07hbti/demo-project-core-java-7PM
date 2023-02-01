6a3a75c0bd619bde0b057ca50d75665dac9e39b5
amandev2023 <devboyzzz@gmailcom>
2023-02-01 22:12:08 +0530

    Assignment 31Jan(Multiple_Condition_Statement_If_And operator)

---
+++ src/conditional_statements/If_Multiple_Condition_Assigenment_31_jan.java
@@ -0,0 +1,42 @@
+package conditional_statements;
+
+public class If_Multiple_Condition_Assigenment_31_jan {
+
+    public static void main(String[] args) {
+
+        //Assignment 31 jan//
+
+        //Brand : IBM//
+        //PRICE <1000 : PRINT Low price product//
+
+        String Brand;
+        int price;
+        Brand = "IBM";
+        price = 900;
+        if (price < 1000 & Brand == "IBM") {
+            System.out.println("Low price product..");
+        } else {
+            System.out.println("Medium price product..");
+        }
+
+        //Brand IBM//
+        //Price >1000 and <5000 : Print Medium price product//
+
+        price = 3500;
+        if (price > 1000 & price < 5000) {
+            System.out.println("Medium price product..");
+        } else {
+            System.out.println("High price product..");
+        }
+
+        //Brand IBM//
+        //Price >5000 : Print High price product//
+
+        price = 6000;
+        if (price > 5000) {
+            System.out.println("High price product..");
+        } else {
+            System.out.println("Very expensive product.");
+        }
+    }
+}
