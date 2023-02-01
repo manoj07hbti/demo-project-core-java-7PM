455a79e1055d7b92ca66d843790f7194e9fab51e
Vishal9528 <vishal8954198936@gmail.com>
2023-02-01 15:15:32 +0530

    Assignment Day 12th NESTED,IF

---
+++ src/condtional_statements/AssignmentDay12th.java
@@ -0,0 +1,33 @@
+package condtional_statements;
+
+public class AssignmentDay12th {
+    public static void main(String[] args) {
+
+        String Brand = "IBM";
+        int Price = 6000;
+
+        if (Brand == "IBM") {
+
+            if (Price <= 1000) {
+
+                System.out.println("Low price product.");
+            }
+
+            if (Price > 1000 & Price < 5000) {
+
+                System.out.println("Medium price product.");
+            }
+
+            if (Price >= 5000) {
+
+                System.out.println("High price product.");
+            }
+        }
+        else {
+
+            System.out.println("Product is not IBM brand.");
+        }
+    }
+}
+
+
