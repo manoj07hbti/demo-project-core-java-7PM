4ff4d1925f3a21deb06f6113b549e8805e54dc32
Ashish <vermaashish732@gmail.com>
2023-02-02 21:21:30 +0530

    write a program which will tell you Day of week based on day number

---
+++ src/conditional_statement/Ashish_Assignment8.java
@@ -0,0 +1,33 @@
+package conditional_statement;
+
+public class Ashish_Assignment8 {
+    public static void main(String[] args) {
+        int day=3;
+        switch (day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednusday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Entered number is invalid!");
+                break;
+        }
+    }
+}
