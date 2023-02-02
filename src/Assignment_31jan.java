787a98afd591c990358ace541e68fa20bd0ab968
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-03 01:00:11 +0530

    Assignment of 31 January

--- src/Assignment_31jan.java
+++ src/Assignment_31jan.java
@@ -1,20 +1,32 @@
 public class Assignment_31jan {
     public static void main(String[] args) {
 
-        String Brand="IBM";
-        int Price=999;
-        int price=4999;
+        String Brand = "IBM";
+        int Price1 = 999;
+        int Price2 = 4999;
+        int Price3 =5500;
 
-        if (Price<1000);
+        if (Price1 < 1000) ;
         System.out.println("Low price product");
 
-        if (Price> 1000 & Price <5000);
+        if (Price1 > 1000 & Price2 < 5001) ;
         System.out.println("Medium price product");
 
-        if (price>5000);
+        if (Price3 > 5000){
+            System.out.println("high value product");
+        }
+        else {
+            System.out.println("Product is Mismatch");
+        }
 
-        System.out.println("high value product");
+        }
 
     }
 
-}
+
+
+
+
+
+
+
