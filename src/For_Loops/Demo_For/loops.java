a236618ff737fc2f6a887dfd71540f8b81d6069a
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-03 04:00:32 +0530

    Assignment of 2nd February

---
+++ src/For_Loops/Demo_For/loops.java
@@ -0,0 +1,19 @@
+package For_Loops.Demo_For;
+
+public class loops {
+
+    public static void main(String[] args) {
+        int number=5;
+
+
+        for(int i=1; i<11; i++){
+
+            System.out.println(number +"X"+ i +"="+ number*i);
+        }
+        }
+
+
+    }
+
+
+
