83842ee092b279b21febf48c18ee158c90b482e7
ravind0303 <ravind.pcf@gamil.com>
2023-02-02 20:10:37 +0530

    switch  Statement

---
+++ src/condtional_statements/Demoswitchstatement.java
@@ -0,0 +1,48 @@
+package condtional_statements;
+
+public class Demoswitchstatement {
+    public static void main(String[] args) {
+        int month;//declared variable of int type
+        month=9;
+        switch(month){
+            case 1:
+                System.out.println("january");
+                break;
+            case 2:
+                System.out.println("february");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+            case 8:
+                System.out.println("august");
+                break;
+            case 9:
+                System.out.println("september");
+                break;
+            case 10:
+                System.out.println("october");
+                break;
+            case 11:
+                System.out.println("november");
+                break;
+            case 12:
+                System.out.println("december");
+                break;
+
+
+        }
+    }
+}
