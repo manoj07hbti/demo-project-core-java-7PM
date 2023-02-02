332b0884a9bb9111dafa1e759b563bd418ec1866
pc user <paramvirsingh127@gmail.com>
2023-02-02 20:33:32 +0530

    switch statement assignment 02 feb

---
+++ src/javaAssignment/Switch_Statement.java
@@ -0,0 +1,68 @@
+package javaAssignment;
+
+public class Switch_Statement {
+    public static void main(String[] args){
+
+        // use to switch statement
+
+        int monthname;
+        monthname= 5;
+
+        switch(monthname){
+
+            case 1:
+                System.out.println("january");
+                break;
+
+            case 2:
+                System.out.println("february");
+                break;
+
+            case 3:
+                System.out.println("march");
+                break;
+
+            case 4:
+                System.out.println("apral");
+                break;
+
+            case 5:
+                System.out.println("may");
+                break;
+
+            case 6:
+                System.out.println("june");
+                break;
+
+            case 7:
+                System.out.println("july");
+                break;
+
+            case 8:
+                System.out.println("august");
+                break;
+
+            case 9:
+                System.out.println("september");
+                break;
+
+            case 10:
+                System.out.println("october");
+                break;
+
+            case 11:
+                System.out.println("november");
+                break;
+
+            case 12:
+                System.out.println("december");
+                break;
+
+            default:
+                System.out.println("number is not match");
+
+
+        }
+
+    }
+}
