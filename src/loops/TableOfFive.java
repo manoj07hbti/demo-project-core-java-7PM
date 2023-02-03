daee725807c8aee7c6817e8d5bf0a2cb51cf59a9
DELL <kumargaurav23097@gmail.com>
2023-02-03 20:00:03 +0530

    Loops 03 Feb  Table_Of_Five

---
+++ src/loops/TableOfFive.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class TableOfFive {
+
+
+    public static void main(String[]args){
+
+        int number=5;
+
+        for(int i=1; i<=10; i++){
+            System.out.println(number + "x" +i+" = " +number*i);
+        }
+
+
+    }
+
+
+
+
+}
