c47704eb742d6ab362ca4fd9fc3eaab68990d3c7
Ashish <vermaashish732@gmail.com>
2023-02-03 19:21:24 +0530

    table example

---
+++ src/loop_statement/table.java
@@ -0,0 +1,12 @@
+package loop_statement;
+
+public class table {
+    public static void main(String[] args) {
+        int number = 2;
+        for (int i=1; i<=10; i++){
+            int tabl = number*i;
+            System.out.println(number + " X " + i + " = " + tabl);
+        }
+
+    }
+}
