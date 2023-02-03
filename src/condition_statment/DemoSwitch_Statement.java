f43c128a377bbb82268d375e3f9eec762708dbc4
DELL <kumargaurav23097@gmail.com>
2023-02-03 13:29:22 +0530

    Switch Statement Use Of String Value

---
+++ src/condition_statment/DemoSwitch_Statement.java
@@ -0,0 +1,45 @@
+package condition_statment;
+
+public class DemoSwitch_Statement {
+
+    public static void main (String[]args){
+
+        int Day = 6;
+
+        switch(Day){
+
+            case 1:
+                System.out.println("Day First :  Monday");
+                break;
+            case 2: System.out.println("Day Second  : Tuesday");
+                 break;
+            case 3:
+                System.out.println("Day Third : Wednesday");
+                break;
+            case 4:
+                System.out.println("Day Fourth  : Thursday");
+                break;
+            case 5:
+                System.out.println("Day Fifth : Friday ");
+                break;
+            case 6:
+                System.out.println("Day Sixth : Saturday");
+                break;
+            case 7:
+                System.out.println("Day Seventh : Sunday ");
+                break;
+            default:
+                System.out.println("Invalid Day Entered");
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
