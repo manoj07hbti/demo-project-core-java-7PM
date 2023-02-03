2e4b00092216b5955ae8cff9627f0b82db17ff5f
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-04 01:56:16 +0530

    Factorial of 7 (Assignment)

---
+++ src/For_Loops/Demo_For/Factorial.java
@@ -0,0 +1,15 @@
+package For_Loops.Demo_For;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        int Fact=1;
+        for (int i=7; i>0; i--) {
+            System.out.println("Fact : " + i*Fact);
+
+
+        }
+    }
+}
+
+
