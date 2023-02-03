77b279fbea503c2eafed6844c512fb74deaad566
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-04 02:15:52 +0530

    Loop demo 2nd feb increment decrement

---
+++ src/Loop/Assigmentloop2nd.java
@@ -0,0 +1,22 @@
+package Loop;
+
+public class Assigmentloop2nd {
+    public static void main(String args[]){
+        // SYNTAX:   for(initialization, condition, decrement) {
+        //     block of statements
+        //    }
+        for(int i=1;  i<7; i++){
+            System.out.println("hii" +i);
+
+
+        }
+         for(int i=10; i>0; i--){
+
+             System.out.println("Decrement Loop"+ i);
+         }
+
+
+
+
+    }
+}
