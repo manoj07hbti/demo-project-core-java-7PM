5742b289f7b88bd6e35546397eeb1e99632a1800
pc user <paramvirsingh127@gmail.com>
2023-02-03 20:29:43 +0530

    for loop conditional Assignment 03 jan

--- src/javaAssignment/For_loop_statement.java
+++ src/javaAssignment/For_loop_statement.java
@@ -4,14 +4,14 @@ public class For_loop_statement {
 
     public static void main(String[] args){
         //SYNTEX: (initialization, condition, increment/decrement)
-        // use of for loop statement
+        // table of 5 and use of for loop statement with increment
 
         int number=5;
         for(int i=1; i<=10; i++){
 
             System.out.println(number+"x"+i+"="+number*i);
         }
-// Use of decrement method
+// Factor of 7 and Use of decrement method
         int fact=1;
 
         for(int i=7; i>0; i--){
