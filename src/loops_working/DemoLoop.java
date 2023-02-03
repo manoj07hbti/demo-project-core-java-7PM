71148abc9c8ad63d62afb8b0e820c2f5bafecc24
60manojkumar@gmail.com <Motivated@2019>
2023-02-03 19:28:24 +0530

    For loop working

---
+++ src/loops_working/DemoLoop.java
@@ -0,0 +1,23 @@
+package loops_working;
+
+public class DemoLoop {
+
+    public static void main(String[] args) {
+
+  // SYNTAX :   for(initialization, condition, increment) {
+        //	            //block of statements
+        //	    }
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println("Hello..."+i);
+        }
+
+
+        for(int i=10; i>0; i-- ){
+
+            System.out.println("Decrement Loop "+i);
+        }
+
+    }
+}
---
+++ src/loops_working/TableOfTwo.java
@@ -0,0 +1,16 @@
+package loops_working;
+
+public class TableOfTwo {
+
+    public static void main(String[] args) {
+
+        int number=2;
+
+        for (int i=1; i<=10; i++){
+
+            System.out.println(number +"X"+i+" = " +number*i);
+        }
+
+
+    }
+}
