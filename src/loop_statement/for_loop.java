94ada3b24535f4b1c9d645d50679c1185dd8a6bd
Ashish <vermaashish732@gmail.com>
2023-02-03 19:20:45 +0530

    for loop

---
+++ src/loop_statement/for_loop.java
@@ -0,0 +1,9 @@
+package loop_statement;
+
+public class for_loop {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println(i + " : Hello");
+        }
+    }
+}
