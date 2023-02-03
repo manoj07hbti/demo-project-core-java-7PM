66a6f04cfb687b67436d85ff323a4c8cf29f6ea5
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-04 01:22:33 +0530

    Loop demo 2nd feb

---
+++ src/Loop/Assigmentloopdemo.java
@@ -0,0 +1,23 @@
+package Loop;
+
+public class Assigmentloopdemo {
+
+    public static void main(String args[]) {
+
+        // SYNTAX:   for(initialization, condition, decrement) {
+            //     block of statements
+            //    }
+            int number=5;
+                   for( int i=1;  i<=10; i++){
+
+                       System.out.println(number+ "X"+i+"="+number*i);
+                   }
+
+
+
+
+
+
+        }
+}
+
