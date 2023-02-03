d6cf14b6a8a75953bee30bde33fa0479b4a60dcb
DELL <kumargaurav23097@gmail.com>
2023-02-03 19:51:03 +0530

    Loops 03 Feb

---
+++ src/loops/Demo_Loops.java
@@ -0,0 +1,30 @@
+package loops;
+
+public class Demo_Loops {
+
+    public static void main(String[]args){
+
+// for(initialization, condition,  increment) {
+//	     //block of statements
+
+
+
+      for( int i=1; i<=10; i++){
+
+          System.out.println(" Increment  Loops"+i);
+      }
+
+// for (initialization, condition,decrement   ) {
+//	     //block of statements
+      for(int i=10; i>0; i--){
+          System.out.println("Decrement Loops"+i);
+      }
+
+
+    }
+
+
+
+
+
+}
