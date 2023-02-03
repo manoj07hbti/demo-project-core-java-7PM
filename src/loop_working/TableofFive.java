e2521c7d17e6dd4abe61c573913badd687f35a6d
ravind0303 <ravind.pcf@gamil.com>
2023-02-03 20:03:03 +0530

    loops work

---
+++ src/loop_working/TableofFive.java
@@ -0,0 +1,15 @@
+package loop_working;
+
+public class TableofFive {
+    public static void main(String[] args) {
+        //SYNTEX: for(initialization'condition'increment){
+        //        //block of statements
+        //   }
+         int number=5;
+
+         for(int j=1; j<=10; j++){
+            System.out.println( number +"*"+j+"=" + number *j);
+        }
+
+    }
+}
