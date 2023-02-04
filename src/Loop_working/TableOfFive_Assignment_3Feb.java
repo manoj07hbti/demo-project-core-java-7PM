3b12420eacd8624d91b49cb2bb63ba6c8379a6c4
Syed <syed5b4@gmail.com>
2023-02-04 19:00:50 +0530

    Assignment 2 Feb

---
+++ src/Loop_working/TableOfFive_Assignment_3Feb.java
@@ -0,0 +1,22 @@
+package Loop_working;
+
+public class TableOfFive_Assignment_3Feb {
+    public static void main(String[] args) {
+        //assignment:print table of 5,
+
+        //syntax: SYNTAX :   for(initialization, condition, increment) {
+        //        //	            //block of statements
+        //        //	    }
+
+        int number=5;
+        //for (int i=1; i<=10; i++){
+        for (int i=1; i<=10; i++){
+
+            System.out.println(number +"X"+i+" = " +number*i);
+        }
+
+
+
+
+    }
+}
