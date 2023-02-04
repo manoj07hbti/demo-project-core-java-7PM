172172f7a990abc6159dc01097198d0841cacf7c
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-05 01:12:24 +0530

    Assigment While loop 1st

---
+++ src/Loop/AssigmentWhileloop.java
@@ -0,0 +1,23 @@
+package Loop;
+
+public class AssigmentWhileloop {
+    public static void main (String[]args){
+
+        int number=100, i=1;
+       while (i<=number){
+            if(i%2==0){
+                System.out.println("Even number" + i);
+
+            }
+            i++;
+        }
+
+
+
+
+
+
+
+
+    }
+}
