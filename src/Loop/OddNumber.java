f5b6918471d97e2cb269a3adacf1fe4d7ab42c1a
Praveen Kumar <66659885+PraveenRaj00@users.noreply.github.com>
2023-02-05 01:11:47 +0530

    Assignment_4th_Feb

---
+++ src/Loop/OddNumber.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        int number=100,i=1;
+        while(i<=number){
+            if(i%2!=0){
+                System.out.println("Odd Number "+i);
+            }
+            i++;
+        }
+    }
+}
