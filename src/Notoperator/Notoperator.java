f94b123e7e3c4b28fc3e28329a8cadfc4c66caf6
Dell <ak0175200@>
2023-02-03 19:03:50 -0800

    Switchmonth

---
+++ src/Notoperator/Notoperator.java
@@ -0,0 +1,19 @@
+package Notoperator;
+
+public class Notoperator {
+
+    public static void main(String[] args) {
+
+        String brand;
+        int price ;
+
+        brand="IBM";
+        price=5000;
+
+        if(price>=1000){
+            System.out.println("low price product");
+        }
+    }
+
+
+}
