49ddf7b187c95a0200ddbfd2543e0debb16b662d
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-05 19:14:14 +0530

    Assigment Loop Even and odd number

--- src/Loop/AssigmentWhileloop.java
+++ src/Loop/AssigmentWhileloop.java
@@ -1,23 +1,53 @@
 package Loop;
 
 public class AssigmentWhileloop {
-    public static void main (String[]args){
+    public static void main(String[] args) {
 
-        int number=100, i=1;
-       while (i<=number){
-            if(i%2==0){
+        int number = 100, i = 1;
+        while (i <= number) {
+            if (i % 2 == 0) {
                 System.out.println("Even number" + i);
 
             }
             i++;
         }
+        int num = 100, j = 1;
+        while (j <= num) {
+            if (!(j % 2 == 0)) {
+                System.out.println("odd number" + j);
+            }
+            j++;
 
 
-
-
-
-
-
+        }
 
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Loop/PrimeNumber.java
@@ -0,0 +1,36 @@
+package Loop;
+
+public class PrimeNumber {
+    public static void main(String [] args){
+        int number=50,i=1;
+        while(i<=number){
+
+            if(! (i%2==0)){
+
+                System.out.println("prime number"+i);
+            }
+           i++;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
