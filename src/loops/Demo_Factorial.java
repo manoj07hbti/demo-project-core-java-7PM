3e8a92d9ad0e58c6ee2c41c3ec0fbde15d0f48e0
DELL <kumargaurav23097@gmail.com>
2023-02-05 14:19:31 +0530

    Factorial_Number8

---
+++ src/loops/Demo_Factorial.java
@@ -0,0 +1,22 @@
+package loops;
+
+public class Demo_Factorial {
+
+    public static void main(String[]args){
+
+        int fact = 1;
+        int number=5 ;
+
+        for (int i = 1; i <=8; i++) {
+            fact= fact*i;
+        System.out.println( fact=fact*i);
+
+
+
+    }
+    }
+}
+
+
+
+
