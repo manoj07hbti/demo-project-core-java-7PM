e28829bc0f3fcb0d59cbb787ce18102ebc735bc1
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-06 06:59:31 +0530

    Odd Number (Assignment)

---
+++ src/For_Loops/Demo_For/Odd_Number.java
@@ -0,0 +1,17 @@
+package For_Loops.Demo_For;
+
+public class Odd_Number {
+    public static void main(String[] args) {
+
+        int number=100, i=1;
+
+        while (i<=number){
+            if (i%2!=0){
+                System.out.println("Odd number is : " +i);
+            }
+            i++;
+        }
+
+
+    }
+}
