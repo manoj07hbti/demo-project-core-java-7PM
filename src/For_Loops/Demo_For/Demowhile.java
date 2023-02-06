c2c31bfb1d5df20336ae8d61e789333dec6bc953
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-06 06:47:34 +0530

    Even Number (Assignment)

--- src/For_Loops/Demo_For/Demowhile.java
+++ src/For_Loops/Demo_For/Demowhile.java
@@ -3,15 +3,15 @@ package For_Loops.Demo_For;
 public class Demowhile {
     public static void main(String[] args) {
 
-        int i = 1;
-        int b = 100;
-        while (i < b) {
-            if (i % 2 == 0) {
-                System.out.println("even number is" + i);
-                i++;
+        int number=100, i=1;
+        while (i<number){
+            if (i%2==0) {
+                System.out.println("even number is : " + i);
             }
-        }
+        i++;
+
     }
 
 
+}
 }
