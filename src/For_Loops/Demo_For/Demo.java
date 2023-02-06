bd84b043443d0e4dba8cbef74e1f963a5f774975
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-06 07:18:49 +0530

    Prime Number (Assignment)

---
+++ src/For_Loops/Demo_For/Demo.java
@@ -0,0 +1,17 @@
+package For_Loops.Demo_For;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int number=50, i=1;
+        while (i<number){
+            if (i%2!=0){
+                System.out.println("Prime Number is : " +i);
+
+            }
+            i++;
+        }
+    }
+}
+
+
