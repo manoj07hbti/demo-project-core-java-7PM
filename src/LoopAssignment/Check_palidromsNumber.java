84eed09e4d9aaaad650ab2a62f4912ef4a2a246d
pc user <paramvirsingh127@gmail.com>
2023-02-07 18:54:39 +0530

    find the odd and even number from 1 to 100 Assignment 4 feb

---
+++ src/LoopAssignment/Check_palidromsNumber.java
@@ -0,0 +1,27 @@
+package LoopAssignment;
+
+public class Check_palidromsNumber {
+
+    public static void main(String[] args){
+
+        // check palindroms number
+        int r;
+        int sum=0;
+        int temp;
+        int n= 222; // it is the number variable to be checked for palindroms number
+        temp= n;
+        while(n>0){
+            r=n%10; // getting remender
+            sum= (sum*10)+r;
+            n=n/10;
+        }
+
+        if(temp==sum){
+            System.out.println("this is a palindroms number");
+        }
+        else{
+            System.out.println("this is not a palindroms number");
+        }
+
+    }
+}
