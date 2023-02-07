834d4310a8b40cb9d47da3d9cda758f0aa0f5610
Ashish <vermaashish732@gmail.com>
2023-02-07 23:48:53 +0530

    prime

--- src/loop_statement/prime_number.java
+++ src/loop_statement/prime_number.java
@@ -2,17 +2,28 @@ package loop_statement;
 
 public class prime_number {
     public static void main(String[] args) {
-        System.out.println("Prime number : ");
+        System.out.println("Prime numbers : ");
         int i;
-        int N= 50;
+        int N = 100;
         int x;
+        boolean flag= true;
         //check for every number from 1 to N
-        for(i=2; i<=N; i++){
-            for (x=2; x<=N; x++){
-                if (i/x==0){
-                    System.out.println("prime number is : "+i);
+        for (i = 2; i <= N; i++) {
+            for (x = 2; x < i; x++) {
+                if (i % x == 0) {
+                    flag = false;
+                    break;
                 }
+                else {
+                    flag = true;
+                }
+            }
+            if (flag) {
+                System.out.print(i +" ");
+            } else {
+                System.out.print("");
             }
         }
     }
 }
+
