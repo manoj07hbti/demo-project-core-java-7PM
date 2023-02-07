bf812de5761878af9e0a4fbfc3c408c15e32431f
Ashish <vermaashish732@gmail.com>
2023-02-07 20:10:27 +0530

    find even number between 1 to 100

---
+++ src/loop_statement/even_number_Assignment.java
@@ -0,0 +1,25 @@
+package loop_statement;
+
+public class even_number_Assignment {
+    public static void main(String[] args) {
+        System.out.println("Using For Loop : ");
+        int number = 100;
+        int startingNumber = 1;
+        for (startingNumber = 1; startingNumber <= number; startingNumber++) {
+            if (startingNumber % 2 == 0) {
+                System.out.println("Even number : " + startingNumber);
+            }
+        }
+        System.out.println();
+        System.out.println("Using While Loop : ");
+        int b = 100;
+        int a = 1;
+        while (a<=b){
+            if (a % 2 == 0) {
+                System.out.println("Even number : " + a);
+            }
+            a++;
+
+        }
+    }
+}
