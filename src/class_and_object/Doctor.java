371db07c15d83fa07cbddbb3de93cd1e4100ed92
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-08 23:44:06 +0530

    class object Doctor.java

---
+++ src/class_and_object/Doctor.java
@@ -0,0 +1,66 @@
+package class_and_object;
+
+public class Doctor{
+
+    //properties
+
+    String Drname="ANIL SARIN";
+    String Department="CHILD CARE";
+    int age=30;
+    double salary=3200.56;
+
+    public static void main(String args[]){
+        // SYNTAX ClassName objectName= new ClassName();
+           Doctor object1=new Doctor();// object created with name object1
+
+           System.out.println("Printing object name"+ object1. Drname);
+           System.out.println("Printing object name " +object1.Department);
+           System.out.println("Printing object age"+ object1.age);
+           System.out.println("Printing object salary"+object1.salary);
+
+           Doctor object2=new Doctor();// object created with name object2
+
+            System.out.println("Printing object2 name"+ object2. Drname);
+           System.out.println("Printing object2 name " +object2.Department);
+           System.out.println("Printing object2 age"+ object2.age);
+           System.out.println("Printing object2 salary"+object2.salary);
+
+          Doctor object3=new Doctor();// object created with name object3
+
+          System.out.println("Printing object3 name"+ object3. Drname);
+          System.out.println("Printing object3 name " +object3.Department);
+          System.out.println("Printing object3 age"+ object3.age);
+          System.out.println("Printing object3 salary"+object3.salary);
+
+          Doctor object4=new Doctor();// object created with name object4
+
+          System.out.println("Printing object4 name"+ object4. Drname);
+          System.out.println("Printing object4 name " +object4.Department);
+          System.out.println("Printing object4 age"+ object4.age);
+          System.out.println("Printing object4 salary"+object4.salary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
