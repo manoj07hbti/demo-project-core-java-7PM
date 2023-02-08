98237de706a9f047c869d22bc57901649879be67
pc user <paramvirsingh127@gmail.com>
2023-02-08 18:40:04 +0530

    find the prime number from 1 to 50 Assignment 4 feb

---
+++ src/LoopAssignment/PrimeNumber.java
@@ -0,0 +1,28 @@
+package LoopAssignment;
+
+public class PrimeNumber {
+
+    public static void main(String[] args){
+
+        // find the prime number from 1 to 50
+        int i = 0;
+        int num=0;
+        String primenumber="";
+      // System.out.println("list of prime number from 1 to 50");
+
+        for ( i=1; i<=50; i++){
+            int counter=0;
+            for(num=i; num>=1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if(counter==2){
+               primenumber= primenumber + i + " ";
+            }
+
+        }
+        System.out.println("Print prime numbers 1 to 50");
+        System.out.println(primenumber);
+    }
+}
