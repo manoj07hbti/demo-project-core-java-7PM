de2c588bceb41b4815ef49c96427ac0282b17a2c
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-08 11:04:51 +0530

    Engineer (Assignment)

---
+++ src/class_and_object/Engineer.java
@@ -0,0 +1,25 @@
+package class_and_object;
+
+import com.sun.org.apache.xpath.internal.operations.Neg;
+import sun.rmi.runtime.NewThreadAction;
+
+public class Engineer {
+
+    String name="Rajendra Singh";
+    String city="Agra";
+    int age=55;
+
+
+    public static void main(String[] args) {
+
+        Engineer obj1= new Engineer();
+        System.out.println("Name is : " +obj1.name);
+        System.out.println("City is : " +obj1.city);
+
+        System.out.println("Age is : " +obj1.age);
+
+
+
+
+    }
+    }
