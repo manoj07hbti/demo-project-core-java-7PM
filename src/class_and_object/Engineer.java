cc92c9846c6261a94cbbbd1f276fae7bcce56895
DELL <kumargaurav23097@gmail.com>
2023-02-09 16:25:35 +0530

    Class_And_Object Engineer Class

--- src/class_and_object/Engineer.java
+++ src/class_and_object/Engineer.java
@@ -25,6 +25,21 @@ public class Engineer {
 
          System.out.println("Engineer Name Is "+engineer.name +": Engineer Age iS "+engineer.age +": Engineer Department "+engineer.dept);
 
+         Engineer obj1 = new Engineer("Mukesh", 24, "IIT");
+
+         System.out.println("Engineer Name "+obj1.name +": Engineer Age "+obj1.age +": Engineer Department ");
+
+         Engineer obj2 = new Engineer("Amit", 23,"CS");
+
+         System.out.println("Engineer Name "+obj2.name +": Engineer Age "+obj2.age +": Engineer Department "+obj2.dept);
+
+         Engineer obj3 =new Engineer("Lucky", 25, "Sa");
+
+         System.out.println("Engineer Name "+obj3.name +": Engineer Age "+obj3.age +": Engineer Department "+obj3.dept);
+
+         Engineer obj4 =new Engineer("Shiva", 20,"Is");
+
+         System.out.println("Engineer Name "+obj4.name +": Engineer Age "+obj4.age +": Engineer Department "+obj4.dept);
         }
 
 
