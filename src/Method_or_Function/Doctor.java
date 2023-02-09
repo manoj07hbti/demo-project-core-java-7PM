a9a787621a7400da69f96838a1d39340a3998e3f
Ashish <vermaashish732@gmail.com>
2023-02-09 19:45:56 +0530

    Method or function

---
+++ src/Method_or_Function/Doctor.java
@@ -0,0 +1,14 @@
+package Method_or_Function;
+
+public class Doctor {
+    public void visit(){
+        System.out.println("This is visit method in doctor class");
+    }
+    public void checkup(){
+        System.out.println("This is checkup method in doctor class");
+    }
+    public void surgery(){
+        System.out.println("This is surgery method in doctor class");
+    }
+
+}
---
+++ src/Method_or_Function/Student.java
@@ -0,0 +1,14 @@
+package Method_or_Function;
+
+public class Student {
+    public void study(){
+        System.out.println("This is study method in student class");
+    }
+    public void eat(){
+        System.out.println("This is eat method in student class");
+    }
+    public void play(){
+        System.out.println("This is play method in student class");
+    }
+
+}
