398239d0e845d0b626d84238cb5d2d4bbaa3cf27
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-11 00:06:22 +0530

    method function Odd number.java

--- src/method_or_function/method_or_function/Method_oddNumber.java
+++ src/method_or_function/method_or_function/Method_oddNumber.java
@@ -1,4 +1,28 @@
 package method_or_function.method_or_function;
 
 public class Method_oddNumber {
+
+    public void  PrintOddNumber(){
+
+        for(int i=1; i<=100;i++){
+
+            if(i%2==0){
+
+            System.out.println("Odd number is"+i);
+            }
+        }
+    }
+     public static void main(String args[]){
+
+         // SYNTAX className objectName = new className();
+
+         Method_oddNumber obj=new Method_oddNumber();
+
+         obj.PrintOddNumber();
+
+
+     }
+
+
+
 }
