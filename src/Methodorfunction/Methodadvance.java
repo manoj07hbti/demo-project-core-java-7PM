116a572a4279a66886aaddc9e13c7a225959bfc6
corejava <imranaoa@gmail.com>
2023-02-10 07:08:06 -0800

    Methodadvance

---
+++ src/Methodorfunction/Methodadvance.java
@@ -0,0 +1,31 @@
+package Methodorfunction;
+
+public class Methodadvance {
+    public void makecube(){
+
+        int number=8;
+
+        int cube=number*number*number;
+
+        System.out.println("cube of 8 is" +cube);
+
+
+    }
+    public void printoddNUMBER(){
+
+
+        for  ( int i=1; i<=100; i++){
+
+            if (!(i % 2 == 0)){
+                System.out.println("odd number is "+i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Methodadvance obj=new Methodadvance();
+        obj.makecube();
+        obj.printoddNUMBER();
+    }
+}
