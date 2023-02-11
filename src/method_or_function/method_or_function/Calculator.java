c724d9a34d65a48f43eca886793ea9280a641fc3
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-11 14:14:11 +0530

    method function Calculator.java

--- src/method_or_function/method_or_function/Calculator.java
+++ src/method_or_function/method_or_function/Calculator.java
@@ -2,7 +2,63 @@ package method_or_function.method_or_function;
 
 public class Calculator {
 
+    // Syntax : access_specifier return_type  method_name (parameter) {CODE..}
 
+    public void add(){
+
+        int j=3;
+        int k=4;
+
+        int sum=j+k;
+
+        System.out.println("Addition j and k"+sum);
+    }
+        public void subtract() {
+
+            int a = 10;
+            int b = 5;
+
+            int sum = a - b;
+
+            System.out.println("Subtract a abd b" + sum);
+        }
+         public void multiply() {
+
+             int m = 12;
+
+             int n = 12;
+
+             int sum = m * n;
+
+             System.out.println("Multiply m and n" + sum);
+         }
+         public void divide(){
+
+           int t =145;
+
+           int s= 12;
+
+           double divide=  (double) t/s;
+
+           System.out.println("Division is "+divide);
+
+         }
+
+         public static void main(String args[]){
+
+             // SYNTAX className objectName = new className();
+
+             Calculator  obj =new Calculator();
+
+             obj.add();
+
+             obj.subtract();
+
+             obj.multiply();
+
+             obj.divide();
+
+         }
 
 
 
