0d18f7f9bd73eb1b3dbaf04647c6382e7ec5d39f
Syed <syeda5b4@gmail.com>
2023-02-12 19:39:49 +0530

    DriverCode1

---
+++ src/class_and_object/DriverCode.java
@@ -0,0 +1,11 @@
+package class_and_object;
+
+public class DriverCode {
+    public static void main(String[] args) {
+        //syntax: className objName=new className(parmet 1, parmet 2 ..);
+
+        Engineer_Assignment_7Feb Engg=new Engineer_Assignment_7Feb();
+        System.out.println("Engg stream " +Engg.Stream +"Engg Company "+Engg.Company);
+
+    }
+}
--- src/method_or_function/Demo.java
+++
@@ -1,7 +0,0 @@
-package method_or_function;
-
-public class Demo {
-    public static void main(String[] args) {
-        String Name="Sted";
-    }
-}
