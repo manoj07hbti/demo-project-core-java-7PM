60b39f051a308e638abbb21a51411176bf1bd705
ASHISH <vermaashish732@gmail.com>
2023-02-13 05:49:44 -0800

    cube withh method input param

---
+++ src/Method_or_Function/methodInputParam.java
@@ -0,0 +1,15 @@
+package Method_or_Function;
+
+public class methodInputParam {
+    // write a program to make a cube of given number
+    public void makecube(int number){
+        int cube = number*number*number;
+        System.out.println("Cube of the given number is : "+cube);
+
+    }
+
+    public static void main(String[] args) {
+        methodInputParam obj= new methodInputParam();
+        obj.makecube(5);
+    }
+}
