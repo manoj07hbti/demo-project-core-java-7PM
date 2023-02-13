ba771ebc531c58250284de4bf3b3fcbf1cd029e7
ASHISH <vermaashish732@gmail.com>
2023-02-13 05:42:56 -0800

    voting

---
+++ src/Method_or_Function/voting.java
@@ -0,0 +1,20 @@
+package Method_or_Function;
+
+public class voting {
+    public void voting(){
+        int age= 28;
+        String city= "Agra";
+        if (age>=18 && city== "Agra"){
+            System.out.println("person is eligible for voting in agra");
+
+        }
+        else {
+            System.out.println("person is not eligible for voting in agra");
+        }
+    }
+
+    public static void main(String[] args) {
+        voting obj = new voting();
+        obj.voting();
+    }
+}
