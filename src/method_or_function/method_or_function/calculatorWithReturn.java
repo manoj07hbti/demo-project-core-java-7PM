5d543c4e63d4452d9a397c9484ab95638b52766c
Krishna Varshney <yadavseeta168@gmail.com>
2023-02-16 16:32:05 +0530

    method function day 4 input return type

---
+++ src/method_or_function/method_or_function/calculatorWithReturn.java
@@ -0,0 +1,53 @@
+package method_or_function.method_or_function;
+
+public class calculatorWithReturn {
+    public static void main(String args[]) {
+        calculatorWithReturn obj = new calculatorWithReturn();
+
+        int sum = obj.add(4, 7);
+
+        System.out.println("Sum is " + sum);
+
+        int sub = obj.subtract(20, 10);
+
+        System.out.println("sub is " + sub);
+
+        int multi = obj.multiply(15, 5);
+
+        System.out.println("multi is " + multi);
+
+        double divide = obj.divide(20, 3);
+
+        System.out.println("divide is " + divide);
+
+    }
+
+    public int add(int a, int b) {
+
+        int sum = a + b;
+
+        return sum;
+    }
+
+    public int subtract(int a, int b) {
+
+        int sub = a - b;
+
+        return sub;
+
+    }
+
+    public int multiply(int a, int b) {
+
+        int multi = a * b;
+
+        return multi;
+    }
+
+    public double divide(int a, int b) {
+
+        double divide = (double) a / (double) b;
+        return divide;
+
+    }
+}
