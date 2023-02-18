a2930e0e56cd19fd5b3bb4a7bdc895ba42316a54
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-02-18 17:57:42 +0530

    Input Parameter..

--- src/method_or_function/Demo.java
+++ src/method_or_function/Demo.java
@@ -4,30 +4,44 @@ import com.sun.org.apache.xpath.internal.objects.XNumber;
 
 public class Demo {
 
-    public void makecube(int Number){
+    public void cube(int number) {
 
-        int cube=Number*Number*Number;
-        System.out.println("number of cube is : " +cube);
+        int cube = number * number * number;
+        System.out.println("cube is : " + cube);
     }
+
     public void voting(int age, String city) {
-        if (age >=18 & city== "Agra") {
-            System.out.println("person is eligibe for voting in agra :");
+
+        if (age > 19 & city == "agra") {
+
+            System.out.println("eligible");
         } else {
-            System.out.println("person is not eligible for voting in agra");
+            System.out.println("not eligible");
         }
     }
 
-    public void calculator(int a, int b){
+    public void multi(int a, int b) {
 
-        int sum=a+b;
-        System.out.println("sum is : " +sum);
+        int multi = a * b;
+        System.out.println("multi is : " + multi);
     }
-        public static void main (String[]args){
-            Demo obj = new Demo();
-            obj.makecube(5);
-            obj.voting(19,"Agra");
-            obj.voting(17,"goa");
-            obj.calculator(25,25);
 
+    public void even(int number) {
+
+        if (number % 2 == 0) {
+        System.out.println("even number : " + number);
         }
-    }
+        else {
+            System.out.println("odd number : " + number);
+        }}
+    public static void main(String[] args) {
+        Demo obj=new Demo() {
+        };
+
+        obj.cube(5);
+        obj.voting(20,"agra");
+        obj.multi(5,8);
+        obj.even(97);
+
+
+        }}
--- src/method_or_function/Input_parameter.java
+++ src/method_or_function/Input_parameter.java
@@ -1,19 +1,31 @@
 package method_or_function;
 
 public class Input_parameter {
-    public void checkprime(int number){
-        if (number %2==0){
-            System.out.println("prime number");
+    public void primenumber(int number){
+
+        boolean flag=true;
+        for (int i=2; i<number; i++);
+        if (number%2==0){
+            System.out.println("number is prime : " +number);
         }
         else {
-            System.out.println("not a prime number");
+            System.out.println("number is not prime : " +number) ;
         }
     }
-    public static void main(String[] args) {
-
-        Input_parameter obj=new Input_parameter();
-
-        obj.checkprime(11);
-
+    public void factorial(int fact){
+        for (int i=fact; i>0; i--){
+           System.out.println("factorial is : " +i);
+        }
     }
-}
+    public void percentage(int number, int ratio){
+        double per=number*ratio/100;
+        System.out.println("answer is : " +per);
+    }
+
+    public static void main(String[] args) {
+        Input_parameter obj = new Input_parameter();
+
+        obj.primenumber(6);
+        obj.factorial(20);
+        obj.percentage(80,10);
+    }}
