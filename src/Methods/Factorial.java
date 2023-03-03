1ec6d42e1b61db81d4dd1f66c4de928afe55e924
Praveen Kumar <66659885+PraveenRaj00@users.noreply.github.com>
2023-03-03 13:45:36 +0530

    Assignment_13th-Feb

---
+++ src/Methods/Factorial.java
@@ -0,0 +1,20 @@
+package Methods;
+
+import com.sun.prism.impl.FactoryResetException;
+
+import java.sql.SQLOutput;
+
+public class Factorial {
+    public void fact(int number){
+        int result=1,i;
+        for(i=number;i>0;i--){
+            result = result*i;
+        }
+        System.out.println("Factoaril of "+number+" is "+result);
+    }
+
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        f.fact(5);
+    }
+}
---
+++ src/Methods/Percentage.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Percentage {
+    int result;
+    public void per(int number , int percent){
+
+        result = number*percent/100;
+        System.out.println(percent+" % of "+number+" is "+result);
+    }
+
+    public static void main(String[] args) {
+        Percentage p = new Percentage();
+        p.per(330,30);
+    }
+}
