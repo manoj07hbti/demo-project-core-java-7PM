75f077a2bb2537387f74ee2b6d9004ff102be291
ASHISH <vermaashish732@gmail.com>
2023-03-05 19:22:56 +0530

    write a program to provie factorial of number

--- src/Test_01/factorial_test.java
+++ src/Test_01/factorial_test.java
@@ -6,13 +6,16 @@ public class factorial_test {
         int fact=1;
         int i;
         Scanner sc= new Scanner(System.in);
+
         System.out.println("write a program to provie factorial of number");
         System.out.println("");
         System.out.print("Enter number : ");
         int num = sc.nextInt();
+
         for (i=1; i<=num; i++){
             fact =fact*i;
         }
+
         System.out.println("Factorial of " +num + " is equal to : " +fact);
     }
 }
