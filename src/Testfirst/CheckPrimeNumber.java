21757d6fdbc1ea80f0ce8e94b66b8b70c529a9e0
pc user <paramvirsingh127@gmail.com>
2023-03-06 17:50:38 +0530

    new logic

--- src/Testfirst/CheckPrimeNumber.java
+++ src/Testfirst/CheckPrimeNumber.java
@@ -1,32 +1,35 @@
 package Testfirst;
 import java.util.*;
 public class CheckPrimeNumber {
-    //Q write a program to check number is prime or not take input from user
+    //Q write a program to check6 number is prime or not take input from user
 
-    public static boolean PrimeNumber(int num){
-        if (num<=1){
-            return false;
-        }
-        for (int i=1; i<Math.sqrt(num); i++){
-            if (num%i ==0){
-                return false;
+    public void PrimeNumber(int num) {
+        int i, m = 0, flag = 0;
+
+        m = num / 2;
+        if (num == 0 || num == 1) {
+            System.out.println(num + "is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (num % i == 0) {
+                    System.out.println(num + "is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(num + "is prime number");
             }
         }
-        return true;
+
+
     }
 
     public static void main(String [] args){
+        CheckPrimeNumber obj = new CheckPrimeNumber();
 
-        Scanner obj= new Scanner(System.in);
-        System.out.println("Enter a Number");
+        obj.PrimeNumber(3);
 
-        int num= obj.nextInt();
-        if (PrimeNumber (num)){
-            System.out.println(num+ " this is not prime number");
-        }
-        else{
-            System.out.println(num+ "this is prime number");
-        }
     }
 
 }
