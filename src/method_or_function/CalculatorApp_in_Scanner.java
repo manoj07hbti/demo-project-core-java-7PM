bb22cadd3bf6307cb67dfef8603b7bc980a9b082
Vishal9528 <vishal8954198936@gmail.com>
2023-03-10 01:48:06 +0530

    Method Day 6 & Calculator App Upgrade

--- src/method_or_function/CalculatorApp_in_Scanner.java
+++ src/method_or_function/CalculatorApp_in_Scanner.java
@@ -34,42 +34,51 @@ public class CalculatorApp_in_Scanner {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter value of A");
-        Scanner scannerObj = new Scanner(System.in);
-        int a = scannerObj.nextInt();
+        int repeat = 0;
 
-        System.out.println("Enter value of B");
-        int b = scannerObj.nextInt();
+        while (repeat == 0) {
 
-        System.out.println("Chose:- 1=ADD,2=SUB,3=MULTI,4=DIVIDE");
-        int chose = scannerObj.nextInt();
+            System.out.println("Enter value of A");
+            Scanner scannerObj = new Scanner(System.in);
+            int a = scannerObj.nextInt();
+
+            System.out.println("Enter value of B");
+            int b = scannerObj.nextInt();
+
+            System.out.println("Chose:- 1=ADD,2=SUB,3=MULTI,4=DIVIDE");
+            int chose = scannerObj.nextInt();
 
 
-        CalculatorApp_in_Scanner obj = new CalculatorApp_in_Scanner();
-        if (chose == 1) {
+            CalculatorApp_in_Scanner obj = new CalculatorApp_in_Scanner();
 
+            if (chose == 1) {
 
-            int add = obj.add(a, b);
-            System.out.println("Add is:- " + add);
+                int add = obj.add(a, b);
+                System.out.println("Add is:- " + add);
+            }
+            else if (chose == 2) {
+
+                int sub = obj.sub(a, b);
+                System.out.println("Subtract is:- " + sub);
+            }
+            else if (chose == 3) {
+
+                int mul = obj.mul(a, b);
+                System.out.println("Multiply is:- " + mul);
+            }
+            else if (chose == 4) {
+
+                double divide = obj.divide(a, b);
+                System.out.println("Divide is:- " + divide);
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("ON=0 AND OFF=ANY NUMBER");
+            repeat = scanner.nextInt();
+
         }
-        else if (chose == 2) {
 
-            int sub = obj.sub(a, b);
-            System.out.println("Subtract is:- " + sub);
-        }
-        else if (chose == 3) {
-
-            int mul = obj.mul(a, b);
-            System.out.println("Multiply is:- " + mul);
-        }
-        else if (chose == 4) {
-
-            double divide = obj.divide(a, b);
-            System.out.println("Divide is:- " + divide);
-        }
 
     }
-
-
 }
 
