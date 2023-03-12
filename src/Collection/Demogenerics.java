ba897b7cfc2711808e8f84cfa7b4d01ca2a2f118
imrankhan <imranaoa@gmail.com>
2023-03-12 12:36:37 +0530

    hashset

---
+++ src/Collection/Demogenerics.java
@@ -0,0 +1,32 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class DemoGeneric
+        public void createArraylist(){
+    ArrayList list = new ArrayList();
+            list.add("java");
+            list.add("java1");
+            list.add("boot strap");
+
+    list.add(78);
+    list.add(13865.7);
+    list.add("photoshop");
+    list.add("html");
+    list.add("css");
+    list.add("Manoj boss");
+    list.add("c++");
+
+
+    System.out.println(list.get(0));
+    System.out.println(list.get(1));
+    System.out.println(list.get(2));
+    System.out.println(list.get(3));
+    System.out.println(list.get(4));
+    System.out.println(list.get(5));
+    System.out.println(list.get(6));
+    System.out.println(list.get(7));
+    System.out.println(list.get(8));
+
+    {
+}
