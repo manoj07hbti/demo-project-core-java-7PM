949220aebec3677a7a926d044862f81baff5cc00
aanchalatre2014 <aanchalatre2014@gmail.com>
2023-03-19 01:48:01 +0530

    HashSet Assignment of 14-Mar-2023

--- src/Model/Employee.java
+++ src/Model/Employee.java
@@ -1,6 +1,8 @@
 // Model class for getting employee details
 package Model;
 
+import java.util.Objects;
+
 public class Employee {
     String Fname, Lname, Dept;
     int EmpId;
@@ -12,6 +14,20 @@ public class Employee {
         Dept = dept;
         EmpId = empId;
     }
+    //Using override on Emp_Id in our custom class to remove redundant data of employees
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return EmpId == employee.EmpId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(EmpId);
+    }
+
     //Getter & Setter for Employee
     public String getFname() {
         return Fname;
